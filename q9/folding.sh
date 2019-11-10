c664da8abbd2af851792cc85791e85a28095ee0d
Christopher Rowley <crowley@gra-login1.graham.sharcnet>
2019-11-10 11:49:38 -0500

    Added new folding simulations using Amber

---
+++ q9/folding.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --gres=gpu:2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --output std.out
+#SBATCH --mem-per-cpu=1024M 
+#SBATCH --account=rrg-crowley-ac
+#SBATCH --time=24:00:00
+#SBATCH --job-name=folding
+
+module purge
+module load nixpkgs/16.09  gcc/7.3.0  cuda/9.2.148  openmpi/3.1.2
+
+module load amber/18.10-18.11
+
+
+#${AMBERHOME}/bin/pmemd.cuda -O -i  -o mdout -p prmtop -c inpcrd -r restrt -x mdcrd -inf mdinfo
+
+${AMBERHOME}/bin/pmemd.cuda -O -p pout -i md-nvt.in -c restrt.eq -o mdout.nvt

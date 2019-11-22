1d10b83f2a80407c6508862c279176ca68b9c4ad
Christopher Rowley <crowley@gra-login3.graham.sharcnet>
2019-11-21 21:41:38 -0500

    Added R scripts to plot profiles without ggplot2 library

---
+++ q2/dipro/plot-simple.R
@@ -0,0 +1,13 @@
+
+pdf("proline.pdf", width=3.25, height=3)
+data=read.table('dipro.colvars.traj',  header=FALSE)
+attach(data)
+
+df = data.frame(V2, V3)
+colnames(df) = c("phi","psi")
+
+#dev.new(width=3.25, height=4, unit="in")
+plot(df$phi, df$psi,   xlab="phi ", ylab="psi", xlim=c(-180,180), ylim=c(-180,180), pch=20, cex=0.1)
+
+dev.off()
+

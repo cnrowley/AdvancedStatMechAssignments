b255f47431a28d1a8f5ab29d7af76bc22c56b309
Christopher Rowley <crowley@gra-login3.graham.sharcnet>
2021-01-28 16:05:19 -0500

    co2 dimer script fixed

--- qm_interaction/co2-dimer.sh
+++ qm_interaction/co2-dimer.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=def-crowley
 #SBATCH --output std.out
 #SBATCH --mem-per-cpu=1024M
-#SBATCH --job-name=h2o-dimer
+#SBATCH --job-name=co2-dimer
 #SBATCH --ntasks-per-node=8
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
 
 module load gaussian
-g16 < h2o-dimer.gjf > h2o-dimer.out
+g16 < co2-dimer.gjf > co2-dimer.out
 

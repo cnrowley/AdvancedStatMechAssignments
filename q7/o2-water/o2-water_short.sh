9cecb181a83b585999aeef0102f2d477b3846fc7
Christopher Rowley <cnrowley@gmail.com>
2021-04-03 21:09:18 -0400

    Updated rrg account and changed q9 analysis to load correct modules and install mdtraj

--- q7/o2-water/o2-water_short.sh
+++ q7/o2-water/o2-water_short.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=rrg-crowley-ac
+#SBATCH --account=rrg-crowley-ab
 #SBATCH --output std.out
 #SBATCH --mem-per-cpu=1024M 
 #SBATCH --mail-type=ALL
--- q9/analysis.sh
+++ q9/analysis.sh
@@ -3,12 +3,16 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --output std.out
 #SBATCH --mem-per-cpu=1024M 
-#SBATCH --account=rrg-crowley-ac
+#SBATCH --account=rrg-crowley-ab
 #SBATCH --time=30:00
 #SBATCH --job-name=folding_analysis
 
 module purge
+
+pip install mdtraj --no-index --user
+module load nixpkgs/16.09
 module load python/3.7.0
+
 module load scipy-stack/2019b
 
 python analysis.py 
